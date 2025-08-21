e873093ab524be7fe0cb211365a354d25415e943
hongphuc111-locketgoldiosuytin <hanoiteamhay@gmail.com>
2025-08-21 13:54:05 +0700

    Update HongPhuc111.js

--- HongPhuc111.js
+++ HongPhuc111.js
@@ -6,7 +6,7 @@ const mapping = {
 var ua = $request.headers["User-Agent"] || $request.headers["user-agent"],
     obj = JSON.parse($response.body);
 
-obj.Attention = "Chào bạn đến với Locket Gold By Hong Phuc!";
+obj.Attention = "Chúc mừng bạn! Vui lòng không bán hoặc chia sẻ cho người khác!";
 
 var locket02 = {
   is_sandbox: !1,
@@ -16,14 +16,14 @@ var locket02 = {
   expires_date: "2099-12-18T01:04:17Z",
   grace_period_expires_date: null,
   unsubscribe_detected_at: null,
-  original_purchase_date: "2025-01-11T00:00:00Z",
-  purchase_date: "2025-01-11T00:00:00Z",
+  original_purchase_date: "2025-09-02T00:00:00Z",
+  purchase_date: "2025-09-02T00:00:00Z",
   store: "app_store"
 };
 
 var locket01 = {
   grace_period_expires_date: null,
-  purchase_date: "2025-01-11T00:00:00Z",
+  purchase_date: "2025-09-02T00:00:00Z",
   product_identifier: "com.locket02.premium.yearly",
   expires_date: "2099-12-18T01:04:17Z"
 };
@@ -33,14 +33,13 @@ const match = Object.keys(mapping).find(e => ua.includes(e));
 if (match) {
   let [e, s] = mapping[match];
   s
-    ? (hongphuc111.product_identifier = s,
-       obj.subscriber.subscriptions[s] = hongphuc111)
-    : obj.subscriber.subscriptions["com.locketgoldbyhongphuc.premium.yearly"] = hongphuc111;
-  obj.subscriber.entitlements[e] = hongphuc111;
+    ? (locket01.product_identifier = s,
+       obj.subscriber.subscriptions[s] = locket02)
+    : obj.subscriber.subscriptions["com.locket02.premium.yearly"] = locket02;
+  obj.subscriber.entitlements[e] = locket01;
 } else {
-  obj.subscriber.subscriptions["com.locketgoldbyhongphuc.premium.yearly"] = hongphuc111;
-  obj.subscriber.entitlements.pro = hongphuc111;
+  obj.subscriber.subscriptions["com.locket02.premium.yearly"] = locket02;
+  obj.subscriber.entitlements.pro = locket01;
 }
 
 $done({ body: JSON.stringify(obj) });
-
